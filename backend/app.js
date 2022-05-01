85ed116999db2a54ff7f048fd9242d80ccd7ac89
Binayak3666 <binayakhotta3666@gmail.com>
2022-05-01 13:49:36 +0530

    formating the code

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Post = require('./models/post')
+const Post = require("./models/post");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://binayakhotta:Spbr1995@freecluster.5ubcq.mongodb.net/mean-angular?retryWrites=true&w=majority")
-          .then(()=>{
-            console.log("Connected to database")
-          }).catch(()=>{
-            console.log("MongoDB connection failed")
-          })
+mongoose
+  .connect(
+    "mongodb+srv://binayakhotta:Spbr1995@freecluster.5ubcq.mongodb.net/mean-angular?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("MongoDB connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 //middleware is app.use
 app.use((req, res, next) => {
@@ -36,27 +39,26 @@ app.post("/api/posts", (req, res, next) => {
   // const post = req.body;
   const post = new Post({
     title: req.body.title,
-    content:req.body.content
-  })
-  post.save()
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({
-    message: 'Post added successfully'
+    message: "Post added successfully",
   });
 });
 
-
-
 app.get("/api/posts", (req, res, next) => {
-  Post.find().then((document)=>{
-    console.log(document)
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: document
+  Post.find()
+    .then((document) => {
+      console.log(document);
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: document,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  }).catch((err)=>{
-    console.log(err)
-  })
-
 });
 //we need to export
-module.exports = app
+module.exports = app;
--- src/app/posts/service/post.service.ts
+++ src/app/posts/service/post.service.ts
@@ -2,46 +2,47 @@ import { Post } from '../model/post.model';
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 // import { response } from 'express';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
   private posts: Post[] = [];
   private updatedPost = new Subject<Post[]>();
 
-  constructor(public http :HttpClient) { }
+  constructor(public http: HttpClient) {}
 
-  getData(){
-    this.http.get<{message:string,posts:any}>("http://localhost:3000/api/posts")
-    .pipe(map((postData)=>{
-      return postData.posts.map(post=>{
-        return {
-          title:post.title,
-          content:post.content,
-          id:post._id
-        }
-      })
-    })).subscribe(
-      (transformPosts)=>{
+  getData() {
+    this.http
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
+      )
+      .subscribe((transformPosts) => {
         this.posts = transformPosts;
-        this.updatedPost.next([...this.posts])
-      }
-    )
+        this.updatedPost.next([...this.posts]);
+      });
   }
-  getPostUpdateListener(){
-    return this.updatedPost.asObservable()
+  getPostUpdateListener() {
+    return this.updatedPost.asObservable();
   }
-  setPost(title : string, content: string){
-    const post  = { id:null, title:title, content:content}
-    this.http.post("http://localhost:3000/api/posts",post).subscribe((res)=>{
+  setPost(title: string, content: string) {
+    const post = { id: null, title: title, content: content };
+    this.http.post('http://localhost:3000/api/posts', post).subscribe((res) => {
       // console.log(res)
       this.posts.push(post);
       this.updatedPost.next([...this.posts]);
-    })
-
+    });
   }
 }
