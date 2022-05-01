72b4749581a34c2c60ad49d82c384184f7b8f06e
Binayak3666 <binayakhotta3666@gmail.com>
2022-05-01 09:02:09 +0530

    create Mongoos Schema and connection to database wit node

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
+const mongoose = require('mongoose')
+
+const Post = require('./models/post')
+
 const app = express();
+
+mongoose.connect("mongodb+srv://binayakhotta:Spbr1995@freecluster.5ubcq.mongodb.net/mean-angular?retryWrites=true&w=majority")
+          .then(()=>{
+            console.log("Connected to database")
+          }).catch(()=>{
+            console.log("MongoDB connection failed")
+          })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,8 +33,12 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  // const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content:req.body.content
+  })
+  post.save()
   res.status(201).json({
     message: 'Post added successfully'
   });
