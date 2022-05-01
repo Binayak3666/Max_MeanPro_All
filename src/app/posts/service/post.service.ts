db0081b0681c5e7cb9c28b00e017ac74f3808936
Binayak3666 <binayakhotta3666@gmail.com>
2022-05-01 09:45:33 +0530

    Transforming Response Data

--- src/app/posts/service/post.service.ts
+++ src/app/posts/service/post.service.ts
@@ -2,6 +2,7 @@ import { Post } from '../model/post.model';
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http';
 // import { response } from 'express';
 
@@ -15,9 +16,18 @@ export class PostService {
   constructor(public http :HttpClient) { }
 
   getData(){
-    this.http.get("http://localhost:3000/api/posts").subscribe(
-      (respon)=>{
-        this.posts = respon["posts"];
+    this.http.get<{message:string,posts:any}>("http://localhost:3000/api/posts")
+    .pipe(map((postData)=>{
+      return postData.posts.map(post=>{
+        return {
+          title:post.title,
+          content:post.content,
+          id:post._id
+        }
+      })
+    })).subscribe(
+      (transformPosts)=>{
+        this.posts = transformPosts;
         this.updatedPost.next([...this.posts])
       }
     )
