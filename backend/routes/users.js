7798183394b7a5043d45fad73078529a655c1608
Binayak3666 <binayakhotta3666@gmail.com>
2022-05-12 07:54:31 +0530

    jwt token created with package installed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -30,6 +31,33 @@ router.post("/signup", (req, res, next) => {
     })
     .catch((err) => {
       console.log(error, "from bcrypt hash");
+    });
+});
+// first we need to validate user id and passowrd is present or not
+// 2nd we have to create JWT token for that we need to in stall jwt package
+// npm install --save jsonwebtoken
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      const token = jwt.sign({ email: user.email, userID: user._id }, 'Unknown@123');
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        message: "Auth failed",
+      });
     });
 });
 
