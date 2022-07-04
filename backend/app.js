d4e9b46c6e356010ccfdd89ecf16f5c90cf87f84
Binayak3666 <binayakhotta3666@gmail.com>
2022-07-04 16:35:23 +0530

    Image set up changes

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join("backend/images")));
 
 //middleware is app.use
 app.use((req, res, next) => {
